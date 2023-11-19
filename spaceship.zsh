17d1033d8c35cff01936826f4044e75f8015f591
sysradium <sysradium@users.noreply.github.com>
2023-11-19 15:07:02 +0100

    remove unnecessary sections from spaceship prompt

--- spaceship.zsh
+++ spaceship.zsh
@@ -1,14 +1,13 @@
 SPACESHIP_DIR_TRUNC_REPO=false
 
+DISABLED_SECTIONS=("azure" "bun" "conda""crystal" "dart" "deno" "dotnet" "elixit" "elm" "erlang" "gcloud" "imbcloud" "java" "lua" "mercurial" "ocaml" "perl" "php" "palumni" "rust" "scala" "swift" "zig")
+for section in "${DISABLED_SECTIONS[@]}"; do
+	spaceship remove $section
+done
 
-# Setting prefix and suffix for docker_context section
 SPACESHIP_DOCKER_CONTEXT_PREFIX="🐳 "
 SPACESHIP_DOCKER_CONTEXT_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"
-
-spaceship remove gcloud
-# Replacing docker with docker_context
 spaceship remove docker
 spaceship add docker_context
 
 spaceship add kubectl
-spaceship remove ansible
