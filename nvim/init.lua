3bdc47e031a0d6bb4e9f7e0c28270183fc7de7ab
sysradium <sysradium@users.noreply.github.com>
2024-07-07 12:41:59 +0200

    remove nvim tree

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,15 +4,7 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 require("config.lazy")
---require("lsp_config")
 require("navigator").setup({ main = true })
--- Some servers have issues with backup files, see #649
-vim.opt.backup = false
-vim.opt.writebackup = false
-
--- Having longer updatetime (default is 4000 ms = 4s) leads to noticeable
--- delays and poor user experience
-vim.opt.updatetime = 80
 
 vim.cmd.colorscheme("catppuccin-mocha")
 
