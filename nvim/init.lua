c9a7646a03e17d43b671b06e3eae5909079a939b
sysradium <sysradium@users.noreply.github.com>
2025-02-15 01:08:07 +0100

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,3 +10,14 @@ vim.api.nvim_set_hl(0, "LineNr", { fg = "#f9e2af", bold = false })
 vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#b6bed9", bold = false })
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloating", { bg = "none" })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	buffer = buffer,
+	callback = function()
+		vim.lsp.buf.code_action({
+			context = { only = { "source.organizeImports" } },
+			apply = true,
+		})
+		vim.wait(100)
+	end,
+})
--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -3,27 +3,6 @@ return {
 	opts = {
 		linters_by_ft = {
 			fish = { "fish" },
-			go = { "golangcilint" },
-		},
-		-- LazyVim extension to easily override linter options
-		-- or add custom linters.
-		---@type table<string,table>
-		linters = {
-			golangcilint = {
-				args = {
-					"run",
-					"--out-format",
-					"json",
-					"--show-stats=false",
-					"--print-issued-lines=false",
-					"--print-linter-name=false",
-					"--no-config",
-					"--disable=depguard",
-					function()
-						return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h")
-					end,
-				},
-			},
 		},
 	},
 }
