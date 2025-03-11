9afd26da698f12b939a204b899ae1d8d60362fff
sysradium <sysradium@users.noreply.github.com>
2025-03-12 00:48:58 +0100

    update

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -2,6 +2,10 @@ return {
 	"saghen/blink.cmp",
 	opts = {
 		completion = {
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 10,
+			},
 			ghost_text = {
 				enabled = true,
 			},
--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -1,8 +1,3 @@
 return {
 	"mfussenegger/nvim-lint",
-	opts = {
-		linters_by_ft = {
-			fish = { "fish" },
-		},
-	},
 }
